924fc59b916a9008d6be8605bad8d52eab3d1fbe
Vinish <mm7920460@gmail.com>
2026-08-22 01:08:37 +0530

    - setup StatusPages.kt to handle unexpected/exception-based errors

--- src/main/kotlin/plugins/StatusPages.kt
+++ src/main/kotlin/plugins/StatusPages.kt
@@ -1,14 +1,27 @@
 package com.vinish.plugins
 
+import com.vinish.model.ErrorResponse
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.BadRequestException
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 
 fun Application.configureStatusPages() {
     install(StatusPages) {
-        exception<Throwable> { call, cause ->
-            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
+
+        exception<BadRequestException> { call, _ ->
+            call.respond(
+                HttpStatusCode.BadRequest,
+                ErrorResponse("Invalid request")
+            )
+        }
+
+        exception<Throwable> { call, _ ->
+            call.respond(
+                HttpStatusCode.InternalServerError,
+                ErrorResponse("Internal server error")
+            )
         }
     }
 }
